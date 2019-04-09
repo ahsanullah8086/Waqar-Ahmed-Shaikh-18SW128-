64bcaf8afef0790e2076a8678ba6622997e6cbf9
ahsanullah8086 <49428597+ahsanullah8086@users.noreply.github.com>
2019-04-09 13:18:50 +0500

    Add files via upload

---
+++ Waqar/Task1.java
@@ -0,0 +1,11 @@
+class Task1
+{
+public static void main(String[]args)
+{
+String w="Waqar Ahmed";
+String r="18SW128";
+int m=860;
+float p=78f;
+System.out.println("name:"+w+"\n"+"Roll no:"+r+"\n"+"Marks="+m+"\n"+"Percentage="+p);
+}
+}
---
+++ Waqar/Task2.java
@@ -0,0 +1,10 @@
+class Task2
+{
+public static void main(String[]args)
+{
+double d=50.25;
+int a=(int)d;
+float f=(float)(d-a);
+System.out.println("Actual value="+d+"\n"+"Integer value="+a+"\n"+"float value="+f);
+}
+}
